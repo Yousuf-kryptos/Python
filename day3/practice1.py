94d1fb66c6dbe6f5b1e1b3c54839b39add3214ad
Yousuf-kryptos <mohammed.yousuf@kryptosinfosys.com>
2026-04-28 18:17:26 +0530

    Add files via upload

---
+++ day3/practice1.py
@@ -0,0 +1,47 @@
+# file = open("demo2.txt","r")
+# content = file.read()
+# print(content)
+# print("file name: ",file.name)
+# print("file Mode: ",file.mode)
+# print("File is closed: ",file.closed)
+# file.close()
+# print("File is closed: ",file.closed)
+
+# import sys
+
+# print(sys.version)
+
+# print("Good",end = " ")
+# print("morning")
+
+# # String
+# x = "Yousuf"
+# print(len(x))
+
+# for x in "banana":
+#     print(x)
+
+# demo = " Yousuf is an B.E Student "
+
+# if "Yousuf" in demo:
+#     print("Yes")
+
+# if "Professional" not in demo:
+#     print("No")
+
+# print(demo[1:5])
+# print(demo[2:])
+# print(demo[:5])
+# print(demo[::-1])
+# print(demo[-5:-2])
+
+# print(demo.upper())
+# print(demo.lower())
+# print(demo.strip())
+# demo2 = demo.replace("Y","y")
+# print(demo2)
+# print(demo.split("."))
+# print(demo.count("u"))
+
+price = 29
+print(f"Price is: {price:.2f}")
